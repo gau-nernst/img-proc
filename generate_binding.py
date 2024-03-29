151c5e180be2aa34fae5c6d0597240ffbf975d1a
Thien Tran <gau.nernst@yahoo.com.sg>
2024-03-29 23:29:50 +0800

    add script to auto-generate binding

---
+++ generate_binding.py
@@ -0,0 +1,137 @@
+import re
+
+KNOWN_ENUM_TYPES = ("Interpolation",)
+
+
+def parse_argument(arg_str: str):
+    arg_type, arg_name = re.match(r"([\w\s]+\s+\*{0,})\s{0,}(\w+)$", arg_str).groups()
+    return arg_type.strip(), arg_name
+
+
+def parse_header_file(filename):
+    funcs = []
+    func_str = ""
+
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+
+            # only support void return type
+            if func_str or line.startswith("void"):
+                func_str += line
+
+                if func_str.endswith(";"):
+                    re_match = re.search(r"void ([\w_]+)\((.+)\);", func_str)
+                    func_name = re_match.group(1)
+                    func_args = [parse_argument(x) for x in re_match.group(2).split(",")]
+
+                    funcs.append((func_name, func_args))
+                    func_str = ""
+
+    return funcs
+
+
+def generate_py_func(func_name: str, func_args: list[tuple[str, str]]):
+    arg_pattern_lookup = {
+        "const char *": "y#",
+        "char *": "y*",
+        "int": "i",
+        "float": "f",
+        "double": "d",
+    }
+
+    c_vars = []
+    c_func_args = []
+    arg_parse_pattern = ""
+
+    for arg_type, arg_name in func_args:
+        if arg_type in KNOWN_ENUM_TYPES:
+            arg_type = "int"
+
+        # read-only buffer
+        if arg_type.startswith("const ") and arg_type.endswith("*"):
+            arg_type = "const char *"
+            c_vars.append((arg_type, arg_name))
+            c_vars.append(("Py_ssize_t", f"{arg_name}_size"))
+            c_func_args.append(arg_name)
+
+        elif arg_type.endswith("*"):
+            arg_type = "char *"
+            c_vars.append(("Py_buffer", arg_name))
+            c_func_args.append(f"{arg_name}.buf")
+
+        elif arg_type in ("int", "float", "double"):
+            c_vars.append((arg_type, arg_name))
+            c_func_args.append(arg_name)
+
+        else:
+            raise ValueError(f"Unsupported {arg_type=} for {arg_name=}")
+
+        arg_parse_pattern += arg_pattern_lookup[arg_type]
+
+    lines = [f"static PyObject *py_{func_name}(PyObject *self, PyObject *args) {{"]
+
+    # declare local variables
+    for c_type, c_name in c_vars:
+        lines.append(f"  {c_type} {c_name};")
+    lines.append("")
+
+    # parse input arguments
+    lines.append(
+        f'  if (!PyArg_ParseTuple(args, "{arg_parse_pattern}", {", ".join("&" + var_name for _, var_name in c_vars)}))'
+    )
+    lines.append("    return NULL;")
+    lines.append("")
+    lines.append("  Py_BEGIN_ALLOW_THREADS;")
+    lines.append(f'  {func_name}({", ".join(c_func_args)});')
+    lines.append("  Py_END_ALLOW_THREADS;")
+    lines.append("")
+    for var_type, var_name in c_vars:
+        if var_type == "Py_buffer":
+            lines.append(f"  PyBuffer_Release(&{var_name});")
+    lines.append("")
+    lines.append("  Py_INCREF(Py_None);")
+    lines.append("  return Py_None;")
+    lines.append("}")
+    return lines
+
+
+def generate_extension_source(header_file):
+    funcs = parse_header_file(header_file)
+
+    lines = [
+        '#include "img_proc.h"',
+        "#define PY_SSIZE_T_CLEAN",
+        "#include <Python.h>",
+        "",
+    ]
+
+    for func_name, func_args in funcs:
+        lines.extend(generate_py_func(func_name, func_args))
+        lines.append("")
+
+    lines.append("static PyMethodDef ImgProcMethods[] = {")
+    for func_name, _ in funcs:
+        lines.append(f'  {{"{func_name}", py_{func_name}, METH_VARARGS, ""}},')
+    lines.append("  {NULL, NULL, 0, NULL},")  # sentinel
+    lines.append("};")
+    lines.append("")
+    lines.append("static struct PyModuleDef img_proc_module = {")
+    lines.append('    PyModuleDef_HEAD_INIT, "img_proc", "module docstring", -1, ImgProcMethods,')
+    lines.append("};")
+    lines.append("")
+    lines.append("PyMODINIT_FUNC PyInit_img_proc(void) { return PyModule_Create(&img_proc_module); }")
+    return lines
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    CURRENT_DIR = Path(__file__).parent
+    filename = Path(__file__).name
+
+    lines = generate_extension_source(CURRENT_DIR / "img_proc.h")
+    with open(CURRENT_DIR / "py_img_proc.c", "w") as f:
+        f.write(f"// This file is auto-generated from {filename}\n")
+        f.write("\n".join(lines))
+        f.write("\n")
--- py_img_proc.c
+++ py_img_proc.c
@@ -1,22 +1,20 @@
-// https://docs.python.org/3/extending/extending.html
+// This file is auto-generated from generate_binding.py
 #include "img_proc.h"
-#include <stdint.h>
-
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 static PyObject *py_image_resize(PyObject *self, PyObject *args) {
-  const uint8_t *image;
-  Py_ssize_t image_size; // unused
-  int width, height, depth, new_width, new_height;
-  Interpolation interpolation;
+  const char * image;
+  Py_ssize_t image_size;
+  int width;
+  int height;
+  int depth;
+  int new_width;
+  int new_height;
+  int interpolation;
   Py_buffer output;
 
-  // y# -> read-only bytes-like object
-  // i  -> int
-  // y* -> bytes-like object
-  if (!PyArg_ParseTuple(args, "y#iiiiiiy*", &image, &image_size, &width, &height, &depth, &new_width, &new_height,
-                        &interpolation, &output))
+  if (!PyArg_ParseTuple(args, "y#iiiiiiy*", &image, &image_size, &width, &height, &depth, &new_width, &new_height, &interpolation, &output))
     return NULL;
 
   Py_BEGIN_ALLOW_THREADS;
@@ -29,16 +27,61 @@ static PyObject *py_image_resize(PyObject *self, PyObject *args) {
   return Py_None;
 }
 
-// method table
+static PyObject *py_get_rotation_matrix_2d(PyObject *self, PyObject *args) {
+  double cx;
+  double cy;
+  double angle;
+  double scale;
+  Py_buffer output;
+
+  if (!PyArg_ParseTuple(args, "ddddy*", &cx, &cy, &angle, &scale, &output))
+    return NULL;
+
+  Py_BEGIN_ALLOW_THREADS;
+  get_rotation_matrix_2d(cx, cy, angle, scale, output.buf);
+  Py_END_ALLOW_THREADS;
+
+  PyBuffer_Release(&output);
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+static PyObject *py_warp_affine(PyObject *self, PyObject *args) {
+  const char * src;
+  Py_ssize_t src_size;
+  int width;
+  int height;
+  int depth;
+  const char * transform;
+  Py_ssize_t transform_size;
+  int new_width;
+  int new_height;
+  int interpolation;
+  Py_buffer dst;
+
+  if (!PyArg_ParseTuple(args, "y#iiiy#iiiy*", &src, &src_size, &width, &height, &depth, &transform, &transform_size, &new_width, &new_height, &interpolation, &dst))
+    return NULL;
+
+  Py_BEGIN_ALLOW_THREADS;
+  warp_affine(src, width, height, depth, transform, new_width, new_height, interpolation, dst.buf);
+  Py_END_ALLOW_THREADS;
+
+  PyBuffer_Release(&dst);
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
 static PyMethodDef ImgProcMethods[] = {
-    {"image_resize", py_image_resize, METH_VARARGS, ""}, //
-    {NULL, NULL, 0, NULL},                               // sentinel
+  {"image_resize", py_image_resize, METH_VARARGS, ""},
+  {"get_rotation_matrix_2d", py_get_rotation_matrix_2d, METH_VARARGS, ""},
+  {"warp_affine", py_warp_affine, METH_VARARGS, ""},
+  {NULL, NULL, 0, NULL},
 };
 
-// module definition
 static struct PyModuleDef img_proc_module = {
     PyModuleDef_HEAD_INIT, "img_proc", "module docstring", -1, ImgProcMethods,
 };
 
-// module initialization
 PyMODINIT_FUNC PyInit_img_proc(void) { return PyModule_Create(&img_proc_module); }
