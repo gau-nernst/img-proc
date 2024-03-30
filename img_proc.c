ee7e1f404f8e835a5c03ce7ddcd92c07a1237255
Thien Tran <gau.nernst@yahoo.com.sg>
2024-03-30 19:01:16 +0800

    fix cross-platform issue

--- img_proc.c
+++ img_proc.c
@@ -1,6 +1,10 @@
 #include "img_proc.h"
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 #define CLAMP(x, low, high) MIN(MAX((x), (low)), (high))
